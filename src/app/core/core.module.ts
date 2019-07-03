3a7e515e5fde4921727501960fa83cce03659fc1
chathurangasandun <csk@tiqri.com>
2019-07-03 22:48:37 +0300

    menu added

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,11 +6,26 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule } from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppNavComponent } from './app-nav/app-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+
+
 @NgModule({
-    imports: [CommonModule,HttpClientModule, RouterModule, ReactiveFormsModule],
-    exports: [ HeaderComponent, FooterComponent, ReactiveFormsModule],
-    declarations: [HeaderComponent, FooterComponent],
+    imports: [CommonModule, HttpClientModule, RouterModule, ReactiveFormsModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatSidenavModule,
+        MatListModule,
+        MatButtonModule,
+        MatIconModule,
+        LayoutModule
+    ],
+    exports: [HeaderComponent, FooterComponent, ReactiveFormsModule, AppNavComponent],
+    declarations: [HeaderComponent, FooterComponent, AppNavComponent,AppNavComponent],
     providers: []
-  })
-  export class CoreModule { }
-  
+})
+export class CoreModule { }
--- src/app/modules/customer-management/customer-management.module.ts
+++ src/app/modules/customer-management/customer-management.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomerRegistrationComponent } from './customer-registration/customer-registration.component';
+import { RouterModule } from '@angular/router';
+import { customerManagementRoutes } from './customer-management.route.ts.routing';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(customerManagementRoutes)
   ],
   declarations: [CustomerRegistrationComponent]
 })
